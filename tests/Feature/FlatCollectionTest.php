6c72451517fbd1899ebca31df57e721270fc166b
Budhi Octaviansyah <budioct@gmail.com>
2024-06-03 14:45:26 +0700

    flattening coolection di laravel

---
+++ tests/Feature/FlatCollectionTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class FlatCollectionTest extends TestCase
+{
+    /**
+     * Flattening
+     * ● Flattening adalah operasi transformasi untuk menjadikan nested collection menjadi flat
+     * jadi jika ada collection nested kita bisa gunakan Flattening
+     *
+     * Zipping Operations
+     * Method               Keterangan
+     * collapse()           Mengubah tiap array di item collection menjadi flat collection
+     * flatMap(function)    Iterasi tiap data, dikirim ke function yang menghasilkan collection, dan diubah menjadi flat collection
+     */
+
+    public function testCollapse(){
+
+
+
+    }
+
+
+}
